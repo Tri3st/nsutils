594a9b3a8e795548bd22b5db4c2e0650d1afc5b2
Martin van Diest <martin.van.diest@gmail.com>
2025-09-19 12:48:12 +0200

    Add pinia to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index';
+import { createPinia } from "pinia";
 import { useAuthStore } from '@/stores/auth'
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 
 // Initialize auth before mounting
 const authStore = useAuthStore();
