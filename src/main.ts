dcab1b468db99354a769af1e738b0ff1b67abd60
Martin van Diest <martin.van.diest@gmail.com>
2025-09-20 12:23:54 +0200

    Improve use of axios
    By giving it an own file.
    Puth the defaults in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,27 @@ import App from './App.vue'
 import router from './router/index';
 import { createPinia } from "pinia";
 import { useAuthStore } from '@/stores/auth'
+import axios from "axios";
 
+// Setup the axios defaults for CSRF protection
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+// CReate the app
 const app = createApp(App);
+
+// Create the pinia store
 const pinia = createPinia();
 
+// Register the router and pinia
 app.use(router);
 app.use(pinia);
 
 // Initialize auth before mounting
 const authStore = useAuthStore();
 
+// Check auth on app mount
 authStore.checkAuth();
 
+// Mount the app
 app.mount('#app');
