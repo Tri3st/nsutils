de7749bf0bb018358b232dd60842d51e8fc5f058
Martin van Diest <martin.van.diest@gmail.com>
2025-10-24 15:42:54 +0200

    iFix some TS errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw, NavigationGuardNext} from 'vue-router'
 import {useAuthStore} from '@/stores/auth'
 import HomePage from '@/components/pages/HomePage.vue'
 import UploadXML from '@/components/pages/UploadXML.vue'
@@ -7,19 +7,21 @@ import ConvertRaw from '@/components/pages/ConvertRaw.vue'
 import Logout from '@/components/pages/Logout.vue'
 import LoginPage from '@/components/pages/LoginPage.vue'
 
-const routes = [
+// By declaring this module, we can add custom properties to the RouteMeta
+declare module 'vue-router' {
+    interface RouteMeta {
+        requiresAuth?: boolean;
+    }
+}
+
+const routes: Array<RouteRecordRaw> = [
     {path: '/', name: 'Home', component: HomePage},
-    {path: '/upload-xml', name: 'UploadXML', component: UploadXML, meta: {requiresAuth: true} as RouteMeta},
-    {path: '/view-uploads', name: 'ViewUploads', component: ViewUploads, meta: {requiresAuth: true} as RouteMeta},
-    {path: '/convert-raw', name: 'ConvertRaw', component: ConvertRaw, meta: {requiresAuth: true} as RouteMeta},
-    {path: '/logout', name: 'Logout', component: Logout, meta: {requiresAuth: true} as RouteMeta},
+    {path: '/upload-xml', name: 'UploadXML', component: UploadXML, meta: {requiresAuth: true}},
+    {path: '/view-uploads', name: 'ViewUploads', component: ViewUploads, meta: {requiresAuth: true}},
+    {path: '/convert-raw', name: 'ConvertRaw', component: ConvertRaw, meta: {requiresAuth: true}},
+    {path: '/logout', name: 'Logout', component: Logout, meta: {requiresAuth: true}},
     {path: '/login', name: 'Login', component: LoginPage}
 ]
-
-// Define route neta type with requiresAuth
-interface RouteMeta {
-    requiresAuth?: boolean;
-}
 
 const router = createRouter({
     history: createWebHistory(),
@@ -27,18 +29,24 @@ const router = createRouter({
 })
 
 // Auth guard
-router.beforeEach(async (to, _, next: any) => {
+router.beforeEach(async (to, _, next: NavigationGuardNext) => {
     const authStore = useAuthStore();
+    // Check auth status on every navigation
     if (authStore.user === null) {
         await authStore.checkAuth();
     }
 
-    // Allow access to login page
-    if (to.meta.requiresAuth && !authStore.user) {
-        next('/login')
-    } else if (to.path === 'login' && authStore.user) {
-        next('/home')
+    const requiresAuth = to.meta.requiresAuth;
+    const isAuthenticated = !!authStore.user;
+
+    if (requiresAuth && !isAuthenticated) {
+        // If route requires auth and user is not authenticated, redirect to login
+        next({ name: 'Login' });
+    } else if (to.name === 'Login' && isAuthenticated) {
+        // If user is authenticated and tries to access login page, redirect to home
+        next({ name: 'Home' });
     } else {
+        // Otherwise, allow navigation
         next();
     }
 })
