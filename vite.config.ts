b4ac4ed22ab023d8cae0c6a1508b5e4f03ed664d
Martin van Diest <martin.van.diest@gmail.com>
2025-11-06 14:55:19 +0100

    Now then?

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import { visualizer } from 'rollup-plugin-visualizer';
 import 'ant-design-vue/dist/reset.css';
-import 'ant-design-vue/dist/antd.css';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
