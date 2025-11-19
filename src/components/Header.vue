196265045e86c4e84027ee4ea3c816338425a934
Martin van Diest <martin.van.diest@gmail.com>
2025-11-19 15:44:36 +0100

    Not finished yet on the navigation.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,6 +24,7 @@ const dropDownItems = [
 </script>
 
 <template>
+  <a-layout-header>
   <nav class="bg-yellow-400 flex items-center justify-between px-6 py-2 shadow-md">
     <!-- Left: NS Logo -->
     <img src="/nslogo.svg" alt="NS Logo" class="h-8 w-auto" />
@@ -35,31 +36,9 @@ const dropDownItems = [
       <NavButton url="/view-uploads" text="View Uploads" type="normal"/>
       <NavButton url="/convert-raw" text="Convert RAW to Image" type="normal"/>
 
-      <!-- Right: Auth Section -->
-      <div class="flex items-center space-x-3">
-        <!-- Show when logged in -->
-        <template v-if="isAuthenticated">
-          <!-- Username + Crown if ADMIN -->
-          <div class="flex items-center space-x-1 font-semibold">
-            <!-- Avatar circle -->
-            <a-tooltip>
-              <NavButton url="/" :text="user!.username.charAt(0).toUpperCase()" type="circle"/>
-            </a-tooltip>
-          <img src="/crown.svg" alt="Admin" class="w-5 h-5" v-if="isAdmin" />
-          </div>
-          <NavButton url="/logout" text="Logout" type="logout"/>
-        </template>
-
-        <!-- Show when logged out -->
-        <template v-else>
-          <NavButton url="/login" text="Login" type="login"/>
-        </template>
-
-        <!-- Banner Image -->
-        <img src="/new_banner_small.jpg" alt="Banner" class="h-12 w-auto" />
-      </div>
 
       <!-- User AVATAR if authenticated -->
+      <img src="/crown.svg" alt="Admin" class="w-5 h-5" v-if="isAdmin" />
       <NavButton
           url=""
           :text="authStore.user.username.charAt(0).toUpperCase()"
@@ -79,7 +58,9 @@ const dropDownItems = [
           url="/login"
           text="Login"
           type="login"/>
-
+        <!-- Banner Image -->
+        <img src="/new_banner_small.jpg" alt="Banner" class="h-12 w-auto" />
     </div>
+    </nav>
   </a-layout-header>
 </template>
